db8b5e1c38174788e8f609b4ba57316285cb71d2
hkiku482 <h.kikuchi482@gmail.com>
2022-10-21 08:10:23 +0000

    presigned url request

--- pages/sender.tsx
+++ pages/sender.tsx
@@ -1,9 +1,7 @@
 import { Button, CircularProgress, FormControl, FormHelperText, Grid, Input, InputLabel, MenuItem, Select, SelectChangeEvent, } from '@mui/material'
-import { Box } from '@mui/system'
 import axios from 'axios'
 import type { NextPage } from 'next'
 import React, { useEffect, useState } from 'react'
-import { v4 } from 'uuid'
 import { GlobalMenu } from '../src/components/GrobalMenu'
 import { Location } from '../src/domains/location'
 import { getLocations } from '../src/usecases/api'
@@ -15,16 +13,6 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     if (renderFlag) {
-      const getPresignedUrl = async (): Promise<void> => {
-        const url: { data: { presigned_url: string } } = await axios.post(
-          process.env.NEXT_PUBLIC_API_ORIGIN + 'presigned_url',
-          {
-            location_id: v4()
-          }
-        )
-        setPresignedUrl(url.data.presigned_url)
-      }
-      getPresignedUrl()
       getLocations(setLocations)
       setInprogress(false)
       return
@@ -37,40 +25,44 @@ const Home: NextPage = () => {
   const [locationId, setLocationId] = useState<string>('')
   const [locationIdError, setLocationIdError] = useState(false)
 
-  const [imageList, setImageList] = useState<FileList>()
-  const [imageListError, setImageListError] = useState(false)
+  const [image, setImage] = useState<string | ArrayBuffer | null>(null)
+  const [imageError, setImageError] = useState<boolean>(false)
 
   const [inProgress, setInprogress] = useState<boolean>(true)
 
   const handleSubmit = async () => {
+    // error handling
     let e = false
     if (locationId === '') {
       e = true
       setLocationIdError(true)
     }
-    if (!imageList) {
+    if (image === null) {
       e = true
-      setImageListError(true)
+      setImageError(true)
     }
     if (e) {
       return
     }
-    const httpRes: { data: { presigned_url: string } } = await axios.post(process.env.NEXT_PUBLIC_API_ORIGIN + 'presigned_url', {
-      location_id: locationId
-    })
-    setPresignedUrl(httpRes.data.presigned_url)
-    console.log(imageList![0])
-    console.log(presignedUrl)
+
+    const httpRes: { data: { presigned_url: string } } = await axios.post(
+      process.env.NEXT_PUBLIC_API_ORIGIN + 'presigned_url',
+      {
+        location_id: locationId
+      }
+    )
+
     await axios.put(
-      presignedUrl,
-      imageList![0],
+      httpRes.data.presigned_url,
+      image,
       {
         headers: {
           'Content-Type': 'image/jpeg',
-          'Content-Encoding': 'base64'
         }
       }
     )
+    setLocationIdError(false)
+    setImageError(false)
   }
 
   return (
@@ -104,13 +96,17 @@ const Home: NextPage = () => {
           )}
         </Grid>
         <Grid item xs={4}>
-          <FormControl fullWidth error={imageListError}>
+          <FormControl fullWidth error={imageError}>
             <Input required onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
               if (event.target.files !== null) {
-                setImageList(event.target.files)
+                const fileReader = new FileReader()
+                fileReader.onload = (e) => {
+                  setImage(fileReader.result)
+                }
+                fileReader.readAsArrayBuffer(event.target.files[0])
               }
-            }} error={imageListError} name='image' type='file' />
-            <FormHelperText hidden={!imageListError}>送信する画像を選択してください</FormHelperText>
+            }} error={imageError} name='image' type='file' />
+            <FormHelperText hidden={!imageError}>送信する画像を選択してください</FormHelperText>
           </FormControl>
         </Grid>
         <Grid item xs={3}>
@@ -122,6 +118,7 @@ const Home: NextPage = () => {
             送信
           </Button>
         </Grid>
+
       </Grid>
     </GlobalMenu >
   )
